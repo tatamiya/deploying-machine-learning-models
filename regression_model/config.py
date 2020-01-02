4b018ffa40dac4807ade6fcc8424c3c27d17576f
Yuji Tamiya <tatamiya@YujinoMacBook-Pro.local>
2020-01-02 19:24:46 +0900

    .py files

---
+++ regression_model/config.py
@@ -0,0 +1,50 @@
+import os
+import pathlib
+
+import pandas as pd
+
+
+pd.options.display.max_rows = 10
+pd.options.display.max_columns = 10
+
+PACKAGE_ROOT = pathlib.Path(__file__).resolve().parent
+TRAINED_MODEL_DIR = PACKAGE_ROOT / 'trained_models'
+DATASET_DIR = PACKAGE_ROOT / 'datasets'
+
+# data
+TESTING_DATA_FILE = 'test.csv'
+TRAINING_DATA_FILE = 'train.csv'
+TARGET = 'SalePrice'
+
+# variables
+FEATURES = ['MSSubClass', 'MSZoning', 'Neighborhood',
+            'OverallQual', 'OverallCond', 'YearRemodAdd',
+            'RoofStyle', 'MasVnrType', 'BsmtQual', 'BsmtExposure',
+            'HeatingQC', 'CentralAir', '1stFlrSF', 'GrLivArea',
+            'BsmtFullBath', 'KitchenQual', 'Fireplaces', 'FireplaceQu',
+            'GarageType', 'GarageFinish', 'GarageCars', 'PavedDrive',
+            'LotFrontage',
+            # this one is only to calculate temporal variable:
+            'YrSold']
+
+# this variable is to calculate the temporal variable,
+# can be dropped afterwards
+DROP_FEATURES = 'YrSold'
+
+# numerical variables with NA in train set
+NUMERICAL_VARS_WITH_NA = ['LotFrontage']
+
+# categorical variables with NA in train set
+CATEGORICAL_VARS_WITH_NA = ['MasVnrType', 'BsmtQual', 'BsmtExposure',
+                            'FireplaceQu', 'GarageType', 'GarageFinish']
+
+TEMPORAL_VARS = 'YearRemodAdd'
+
+# variables to log transform
+NUMERICAL_LOG_VARS = ['LotFrontage', '1stFlrSF', 'GrLivArea']
+
+# categorical variables to encode
+CATEGORICAL_VARS = ['MSZoning', 'Neighborhood', 'RoofStyle', 'MasVnrType',
+                    'BsmtQual', 'BsmtExposure', 'HeatingQC', 'CentralAir',
+                    'KitchenQual', 'FireplaceQu', 'GarageType', 'GarageFinish',
+                    'PavedDrive']
