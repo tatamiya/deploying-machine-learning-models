065c2429daa0390de3031f1975590a166ed06a80
Yuji Tamiya <tatamiya@YujinoMacBook-Pro.local>
2020-01-03 17:47:16 +0900

    predict.py and data_management.py

---
+++ regression_model/predict.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+
+import config
+
+from data_management import load_pipeline
+
+
+pipeline_file_name = 'regression_model.pkl'
+_price_pipe = load_pipeline(file_name=pipeline_file_name)
+
+
+def make_prediction(*, input_data) -> dict:
+    """Make a predction using the saved model pipeline."""
+    
+    data = pd.read_json(input_data)
+    prediction = _price_pipe.predict(data[config.Features])
+    output = np.exp(prediction)
+    response = {'predictions': output}
+    
+    return response
--- regression_model/train_pipeline.py
+++ regression_model/train_pipeline.py
@@ -1,27 +1,9 @@
 import numpy as np
-import pandas as pd
 from sklearn.model_selection import train_test_split
-import joblib
 
 import pipeline
-# from .data_management import load_dataset, save_pipeline
+from data_management import load_dataset, save_pipeline
 import config
-
-
-def load_dataset(*, file_name: str
-                 ) -> pd.DataFrame:
-    _data = pd.read_csv(f'{config.DATASET_DIR}/{file_name}')
-    return _data
-
-
-def save_pipeline(*, pipeline_to_persist) -> None:
-    """Persist the pipeline."""
-
-    save_file_name = 'regression_model.pkl'
-    save_path = config.TRAINED_MODEL_DIR / save_file_name
-    joblib.dump(pipeline_to_persist, save_path)
-
-    print('saved pipeline')
 
 
 def run_training() -> None:
