afc4f5127b2bda0d46ba4b5e552921fd88d6a35b
Yuji Tamiya <tatamiya@YujinoMacBook-Pro.local>
2020-01-05 16:58:56 +0900

    Section 7.6 API Scema Validation

--- ml_api/tests/test_controller.py
+++ ml_api/tests/test_controller.py
@@ -37,12 +37,13 @@ def test_prediction_endpoint_returns_prediction(flask_test_client):
     post_json = test_data[0:1].to_json(orient='records')
     
     # When
-    response = flask_test_client.post('/v1/predict/regression', json=post_json)
+    response = flask_test_client.post('/v1/predict/regression',
+                                      json=json.loads(post_json))
     
     # Then
     assert response.status_code == 200
     response_json = json.loads(response.data)
     prediction = response_json['predictions']
     response_version = response_json['version']
-    assert math.ceil(prediction) == 112476
+    assert math.ceil(prediction[0]) == 112476
     assert response_version == _version
