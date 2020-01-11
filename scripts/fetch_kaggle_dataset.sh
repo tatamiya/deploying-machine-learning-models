835c5243e46e3349a8a7fa92eb007dfc7dc18687
Yuji Tamiya <tatamiya@YujinoMacBook-Pro.local>
2020-01-11 15:56:13 +0900

    Section 8.3 Setup to GitActions Config
    modified .yml to utilize environmental variables
    
    fixed type: path for tests dir
    
    push trained_models
    
    modified the timing of pytest
    
    unzip the data set downloaded from kaggle.
    
    unzip datasets
    
    stop installing unzip because it was not needed.
    
    fixed typo: dir of .zip file to unzip.
    
    fixed typo: dir to unzip data sets.
    
    fixed typo: env variable for datasets path

---
+++ scripts/fetch_kaggle_dataset.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+export DIR_DATASETS=regression_model/regression_model/datasets/
+
+kaggle competitions download -c house-prices-advanced-regression-techniques -p $DIR_DATASETS 
+unzip $DIR_DATASETS*.zip -d $DIR_DATASETS
