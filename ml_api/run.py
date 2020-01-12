e9bd90b30f79060399b256e3820bfb64749d4ede
tatamiya <tamyuji1002@gmail.com>
2020-01-12 17:29:07 +0900

    Section 11.3: Dockerfile Setup

--- ml_api/run.py
+++ ml_api/run.py
@@ -1,8 +1,8 @@
 from api.app import create_app
-from api.config import DevelopmentConfig
+from api.config import DevelopmentConfig, ProductionConfig
 
 
-application = create_app(config_object=DevelopmentConfig)
+application = create_app(config_object=ProductionConfig)
 
 if __name__ == '__main__':
     application.run()
