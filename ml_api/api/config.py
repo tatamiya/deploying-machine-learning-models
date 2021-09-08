d8172f89c2118bbd8ad0e0a05910887e4633503c
tatamiya <tamyuji1002@gmail.com>
2021-09-08 20:50:03 +0900

    fix typo

--- ml_api/api/config.py
+++ ml_api/api/config.py
@@ -51,7 +51,7 @@ class Config:
 
 class ProductionConfig(Config):
     DEBUG = False
-    SERVER_ADDRESS: os.environ.get("SEVER_ADDRESS", "0.0.0.0")
+    SERVER_ADDRESS = os.environ.get("SERVER_ADDRESS", "0.0.0.0")
     SERVER_PORT = os.environ.get("SERVER_PORT", "5000")
 
 
