13379ee4135ee809b65d8d992175e462a76233c2
tatamiya <tamyuji1002@gmail.com>
2021-09-13 17:54:33 +0900

    fix typo

--- regression_model/regression_model/processing/validation.py
+++ regression_model/regression_model/processing/validation.py
@@ -22,7 +22,7 @@ def validate_inputs(input_data: pd.DataFrame) -> pd.DataFrame:
 
     # check for values <= 0 for the log transformed variables
     if (input_data[config.NUMERICAL_LOG_VARS] <= 0).any().any():
-        vars_with_neg_values = config.NUMERICALS_LOG_VARS[
+        vars_with_neg_values = config.NUMERICAL_LOG_VARS[
             (input_data[config.NUMERICAL_LOG_VARS] <= 0).any()
         ]
         validated_data = validated_data[validated_data[vars_with_neg_values] > 0]
