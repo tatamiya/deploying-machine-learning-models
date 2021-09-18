6ff44474c8b507defa2c1d5c54da8840d87c32f4
tatamiya <tamyuji1002@gmail.com>
2021-09-18 18:23:58 +0900

    clean: import uvicorn when it is used

--- ml_api/run.py
+++ ml_api/run.py
@@ -1,9 +1,9 @@
-import uvicorn
-
 from api.app import create_app
 from api.config import ProductionConfig
 
 application = create_app(config_object=ProductionConfig)
 
 if __name__ == "__main__":
+    import uvicorn
+
     uvicorn.run(application, host="0.0.0.0", port=5000)
