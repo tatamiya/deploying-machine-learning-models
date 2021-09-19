5db389a670d9ec82b4b317cee34e12dbff514871
tatamiya <tamyuji1002@gmail.com>
2021-09-19 13:18:22 +0900

    clean: make the input test data explicit

--- ml_api/tests/test_controller.py
+++ ml_api/tests/test_controller.py
@@ -1,8 +1,4 @@
-import json
-
 from regression_model import __version__ as _version
-from regression_model.config import config as model_config
-from regression_model.processing.data_management import load_dataset
 
 from ..api import __version__ as api_version
 
@@ -28,16 +24,94 @@ def test_version_endpoint_returns_version(test_client):
 
 
 def test_prediction_endpoint_returns_prediction(test_client):
-    # Given
-    # Load the test data from the regression_model package
-    # This is important as it makes it harder for the test
-    # data versions to get confused by not spreading it
-    # across packages.
-    test_data = load_dataset(file_name=model_config.TESTING_DATA_FILE)
-    post_json = test_data[0:1].to_json(orient="records")
+    # When
+    test_data = [
+        {
+            "Id": 1461,
+            "MSSubClass": 20,
+            "MSZoning": "RH",
+            "LotFrontage": 80.0,
+            "LotArea": 11622,
+            "Street": "Pave",
+            "Alley": None,
+            "LotShape": "Reg",
+            "LandContour": "Lvl",
+            "Utilities": "AllPub",
+            "LotConfig": "Inside",
+            "LandSlope": "Gtl",
+            "Neighborhood": "NAmes",
+            "Condition1": "Feedr",
+            "Condition2": "Norm",
+            "BldgType": "1Fam",
+            "HouseStyle": "1Story",
+            "OverallQual": 5,
+            "OverallCond": 6,
+            "YearBuilt": 1961,
+            "YearRemodAdd": 1961,
+            "RoofStyle": "Gable",
+            "RoofMatl": "CompShg",
+            "Exterior1st": "VinylSd",
+            "Exterior2nd": "VinylSd",
+            "MasVnrType": "None",
+            "MasVnrArea": 0.0,
+            "ExterQual": "TA",
+            "ExterCond": "TA",
+            "Foundation": "CBlock",
+            "BsmtQual": "TA",
+            "BsmtCond": "TA",
+            "BsmtExposure": "No",
+            "BsmtFinType1": "Rec",
+            "BsmtFinSF1": 468.0,
+            "BsmtFinType2": "LwQ",
+            "BsmtFinSF2": 144.0,
+            "BsmtUnfSF": 270.0,
+            "TotalBsmtSF": 882.0,
+            "Heating": "GasA",
+            "HeatingQC": "TA",
+            "CentralAir": "Y",
+            "Electrical": "SBrkr",
+            "1stFlrSF": 896,
+            "2ndFlrSF": 0,
+            "LowQualFinSF": 0,
+            "GrLivArea": 896,
+            "BsmtFullBath": 0.0,
+            "BsmtHalfBath": 0.0,
+            "FullBath": 1,
+            "HalfBath": 0,
+            "BedroomAbvGr": 2,
+            "KitchenAbvGr": 1,
+            "KitchenQual": "TA",
+            "TotRmsAbvGrd": 5,
+            "Functional": "Typ",
+            "Fireplaces": 0,
+            "FireplaceQu": None,
+            "GarageType": "Attchd",
+            "GarageYrBlt": 1961.0,
+            "GarageFinish": "Unf",
+            "GarageCars": 1.0,
+            "GarageArea": 730.0,
+            "GarageQual": "TA",
+            "GarageCond": "TA",
+            "PavedDrive": "Y",
+            "WoodDeckSF": 140,
+            "OpenPorchSF": 0,
+            "EnclosedPorch": 0,
+            "3SsnPorch": 0,
+            "ScreenPorch": 120,
+            "PoolArea": 0,
+            "PoolQC": None,
+            "Fence": "MnPrv",
+            "MiscFeature": None,
+            "MiscVal": 0,
+            "MoSold": 6,
+            "YrSold": 2010,
+            "SaleType": "WD",
+            "SaleCondition": "Normal",
+        },
+    ]
 
     # When
-    response = test_client.post("/v1/predict/regression", json=json.loads(post_json))
+    response = test_client.post("/v1/predict/regression", json=test_data)
 
     # Then
     assert response.status_code == 200
